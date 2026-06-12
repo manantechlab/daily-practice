3eb966d81b4874f12bb490e1205af908aa33035e
MANAN PATEL <mananp.connect@gmail.com>
2026-06-12 23:04:54 +0530

    Set Mismatch

---
+++ setimismatch.java
@@ -0,0 +1,30 @@
+public class setimismatch {
+    public int[] findErrorNums(int[] nums) {
+        int dup = -1, missing = -1;
+
+        for(int i =1; i<=nums.length; i++){
+            int count =0;
+            for(int j = 0; j < nums.length; j++){
+                if(nums[j] == i){
+                    count++;
+                }
+            }
+            if(count == 2){
+                dup = i;
+            }
+            else if(count == 0){
+                missing = i;
+            }
+        }
+        return new int[] {dup,missing};
+    }
+    public static void main(String[] args) {
+        setimismatch obj = new setimismatch();
+
+        int[] nums = {1, 2, 2, 4};
+        int[] result = obj.findErrorNums(nums);
+
+        System.out.println("Duplicate number: " + result[0]);
+        System.out.println("Missing number: " + result[1]);
+    }
+}
