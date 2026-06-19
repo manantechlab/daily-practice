5fb5462afb1652e542bde62a1b53dd0fbcf2aab2
MANAN PATEL <mananp.connect@gmail.com>
2026-06-19 23:39:14 +0530

    Largest Rectangle in Histogram

---
+++ largestrec.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+class largestrec {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer>stack = new Stack<>();
+        int maxArea = 0;
+        int n  = heights.length;
+
+        for (int i = 0; i<=n; i++){
+            int currentHeight;
+
+            if(i==n){
+                currentHeight = 0;
+            }else{
+                currentHeight = heights[i];
+            }
+            while(!stack.isEmpty() && currentHeight < heights[stack.peek()]){
+                int height = heights[stack.pop()];
+                int width;
+
+                if(stack.isEmpty()){
+                    width = i;
+                }else{
+                    width = i - stack.peek()-1;
+                }
+                int area = height * width;
+
+                maxArea = Math.max(maxArea, area);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+    public static void main(String[] args) {
+        largestrec solution = new largestrec();
+        int[] heights = {2, 1, 5, 6, 2, 3};
+        int maxArea = solution.largestRectangleArea(heights);
+        System.out.println("The largest rectangle area is: " + maxArea);
+    }
+}
